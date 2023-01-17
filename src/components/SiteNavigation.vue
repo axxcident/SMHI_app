3de563548baf736f8d10d5a096e77ab0f731b693
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-01-17 19:37:44 +0100

    Start vid11

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -12,7 +12,8 @@
         <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
           @click="toggleModal">
         </i>
-        <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+        <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="addCity"
+          v-if="route.query.preview"></i>
       </div>
 
       <BaseModal v-bind:modalActive="modalActive" @close-modal="toggleModal">
@@ -53,8 +54,36 @@
 
 <script setup>
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { uid } from "uid";
+import { RouterLink, useRoute, useRouter } from 'vue-router';
 import BaseModal from './BaseModal.vue';
+
+const savedCities = ref([]);
+const route = useRoute();
+const router = useRouter();
+const addCity = () => {
+  if (localStorage.getItem('savedCities')) {
+    savedCities.value = JSON.parse(localStorage.getItem("savedCities"));
+  }
+  const locationObj = {
+    id: uid(),
+    // Denna annorlunda eftersom i video är det state: route.params.country. Har ej dock detta pga obv.
+    city: route.params.city,
+    country: route.params.country,
+    coords: {
+      lat: route.query.lat,
+      lng: route.query.lng
+    }
+  };
+
+  savedCities.value.push(locationObj);
+  localStorage.setItem("savedCities", JSON.stringify(savedCities.value));
+
+  let query = Object.assign({}, route.query);
+  delete query.preview;
+  router.replace({ query })
+};
+
 
 const modalActive = ref(null)
 const toggleModal = () => {
